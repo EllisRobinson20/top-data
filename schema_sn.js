642972deb7ae191efba8d79a5c28f593e892da69
L EE <lee@MacBook-Pro.local>
2020-11-25 16:19:23 +0000

    commit from 25th November 2020

--- schema_sn.js
+++ schema_sn.js
@@ -1,6 +1,7 @@
 mongo = Mongo('localhost');
 //Create DB
 socialnetworkDB = mongo.getDB('social_network');
+
 //check collections
 collections = socialnetworkDB.getCollectionNames();
 print("Database Name: social_network ... Showing Initial Collections: ");
@@ -13,18 +14,25 @@ collections_array.forEach(collection => socialnetworkDB.createCollection(collect
 collections = socialnetworkDB.getCollectionNames();
 print("After Creation of collections: ");
 printjson(collections);
+/*stats = socialnetworkDB.stats
+printjson("STATS:\n"+stats)*/
+
 // create users
-printjson(db.getUsers());
 admins_array = ["lee", "james", "lucy"];
-admins_array.forEach(user => {
-	socialnetworkDB.creatUser(
+admins_array.forEach(adminUser => addUserIfExists(adminUser))
+function addUserIfExists(user) {
+	if(!socialnetworkDB.getUser(user)) {
+		print("creating user " + user)
+		socialnetworkDB.createUser(
 		{
 			user: user,
 			pwd: "%P@55w0rd",
-			roles: ["readWrite", "dbAdmin"]
-		}
-	)
-});
-printjson(db.getUsers());
+			roles: ["readWrite", "dbAdmin"/*,"readWriteAnyDatabase","dbAdminAnyDatabase","clusterAdmin"*/ ]
+		})
+	} else {
+		print(user+" already exists")
+	}
+}
+printjson(socialnetworkDB.getUsers());
 
 
