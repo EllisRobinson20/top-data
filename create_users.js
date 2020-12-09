05d01cc577398fd6d2d05f7f2d9c7fd4f98804ff
L EE <lee@MacBook-Pro-2.local>
2020-12-09 16:19:39 +0000

    this now updates the messages fields of users NOTE: it does not solve the issue of adding more messages to the system there are at least messages in the system

--- create_users.js
+++ create_users.js
@@ -2,12 +2,13 @@ mongo = Mongo('localhost');
 db = mongo.getDB('social_network')
 //alter user base to add more users
 //don forget!! must call: "mongo first-names.json create_users.js" in command line
-userBase = 100
+userBase = 10
 months = ["january","february","march","april","may,","june","july","august","september","october","november","december"]
 userObject = {}
 userObjectArray = []
 profileObject = {}
 friendsObject = {}
+messageThreads = []
 for (var i=0;i<=userBase;i++) {
 	var firstName = names[Math.floor(Math.random() * names.length)]
 	var surname = names[Math.floor(Math.random() * names.length)]
@@ -16,7 +17,7 @@ for (var i=0;i<=userBase;i++) {
 	 
 	if (userName || userName != null) {
 		profileObject = createProfileObject()
-		userObject = {"username":userName,"firstname":firstName,"surname":surname, "password":firstName+"2020", "profile":{profileObject}, "friends":[]}
+		userObject = {"username":userName,"firstname":firstName,"surname":surname, "password":firstName+"2020", "profile":{profileObject}, "friends":[], "messageThreads":[]}
 		userObjectArray.push(userObject)
 		db.Members.ensureIndex({userName: 1}, {unique: true}); //may need to change to create index: https://docs.mongodb.com/manual/reference/method/db.collection.createIndex/#db.collection.createIndex
 		printjson(db.Members.getIndexes()) 
--- test_data.js
+++ test_data.js
@@ -3,6 +3,19 @@ db = mongo.getDB("social_network")
 membersColl = db.getCollection("Members")
 cursor = membersColl.find()
 cursor.forEach(function(user){
+	addFriend(user)
+})
+//then add messages for each user from their corresponding friends list
+membersColl = db.getCollection("Members")
+cursor = membersColl.find()
+cursor.forEach(function(user){
+	addMessageData(user)
+})
+////Functions
+function randomNumber(from,to){
+	return Math.floor(Math.random() * to) + from
+}
+function addFriend(user){
 	friendsObject = user.friends
 	friends = Object.keys(friendsObject)
 	anyFriends = friends.length !== 0
@@ -19,17 +32,37 @@ cursor.forEach(function(user){
 
 		})
 	}
-})
-membersColl = db.getCollection("Members")
-cursor = membersColl.find()
-cursor.forEach(function(user){
-	printjson(user)
-})
-function randomNumber(from,to){
-	return Math.floor(Math.random() * to) + from
 }
-function addFriend(user){
-	//var cursor = 
+function  addMessageData(user){
+	random = randomNumber(0,48)
+	date = getDateTime()
+// message data could be complex. many users conversations to deal with and then sender of message and receiveer of message to deal with 
+// messages collections could be seperate
+// or could be ambedded...
+//if embedded... 
+//messages field, containing an object called threads. in threads object each instance has a key of the friend who is recipient in the message thread
+var cursor = membersColl.find()
+cursor.forEach(function(member) {
+	usersFriends = member.friends
+	var friends = usersFriends.map(function(friend) {return friend})
+		friends.forEach(function(friend) {
+
+			//var friendContextExsists = membersColl.find({$and:[{"messageThreads.thread_id": friend.id}, {"messageThreads.thread_id": friend}] })
+
+
+			print("<<<<<<<<<<<<<<<<< friends are: >>>>>>>>>>>>>>>>>>>>>>")
+		
+			membersColl.update({
+				username: user.username
+			},{
+				$push: {
+					messageThreads: {$each:[{'thread_id':friend.id, 'friend_username':friend.username, 'date_added':date, 'message_context':[messages[randomNumber(0,48)]]}]}}})
+		})	
+})
+print("<<<<<<<<<<<<<<<<< users are: >>>>>>>>>>>>>>>>>>>>>>")
+printjson(user)
+print("<<<<<<<<<<<<<<<<< threads are: >>>>>>>>>>>>>>>>>>>>>>")
+printjson(user.messageThreads)
 }
 function getDateTime(){
 	var currentdate = new Date(); 
